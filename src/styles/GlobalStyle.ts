aed324292f2a806ec43d5ef320aa09365d9e80ee
Ricardo Gava <ricardoagava@gmail.com>
2022-08-25 07:51:16 -0300

    initial commit

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    background-color: red;
+    list-style: none;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+`
+
+export default GlobalStyle
