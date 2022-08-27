80ded1a78201feaf422eaa989e17d9179c67a5d8
Ricardo Gava <ricardoagava@gmail.com>
2022-08-27 02:14:30 -0300

    feat(theme): added a default theme for the whole page
    using global styled component to pass the font family and the background color to the whole page

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,10 +4,14 @@ const GlobalStyle = createGlobalStyle`
   * {
     padding: 0;
     margin: 0;
-    background-color: red;
     list-style: none;
     box-sizing: border-box;
     text-decoration: none;
+    font-family: ${({ theme }) => theme.font.family};
+  }
+  
+  body {
+    background-color: ${({ theme }) => theme.colors.primary};
   }
 `
 
---
+++ src/types/styled.d.ts
@@ -0,0 +1,25 @@
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: '#242424',
+      secondary: '#ED1CA6',
+      sec: '#7D00FF',
+      white: '#FFFFFF',
+      gray: {
+        100: '#DFE3EB',
+        500: '#A5A5A5',
+      },
+    },
+    font: {
+      family: 'Inter',
+      weigths: {
+        regular: 400,
+        medium: 500,
+        semiBold: 600,
+        bold: 700
+      },
+    }
+  }
+}
+
+export { }
