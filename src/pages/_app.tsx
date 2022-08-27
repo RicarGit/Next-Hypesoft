8a65c91f3d8a25ca4891f142f0ad5584fd3792e0
Ricardo Gava <ricardoagava@gmail.com>
2022-08-27 02:23:59 -0300

    style(fonts): added Inter font and their weights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import type { AppProps } from 'next/app'
-import GlobalStyle from '../styles/GlobalStyle'
+import Head from 'next/head'
+
+import GlobalStyle from 'styles/GlobalStyle'
+import defautTheme from 'styles/defaultTheme'
+
+import { ThemeProvider } from 'styled-components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={defautTheme}>
       <GlobalStyle />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
