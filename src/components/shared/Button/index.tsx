8cf5a762a632a8eaf5c740188dbb85dbe701c816
Ricardo Gava <ricardoagava@gmail.com>
2022-08-28 03:51:16 -0300

    feat(button/component): created a button component
    created a shared button to use it on other areas

---
+++ src/components/shared/Button/index.tsx
@@ -0,0 +1,19 @@
+import * as S from './styles'
+
+import Image from 'next/image'
+import whiteArrow from "./white-arrow.svg"
+
+type Button = {
+  children: string
+}
+
+const Button = ({ children }: Button) => {
+  return (
+    <S.Container>
+      <S.Label>{children}</S.Label>
+      <Image src={whiteArrow} />
+    </S.Container>
+  )
+}
+
+export default Button
---
+++ src/components/shared/Button/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Container = styled.button`
+  width: 162px;
+  display: flex;
+  justify-content: center;
+  padding: 0 32px;
+  border: none;
+  border-radius: 4px;
+  gap: 8px;
+  background-color: ${({ theme }) => theme.colors.sec};
+`
+
+export const Label = styled.p`
+  display: flex;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 56px;
+  letter-spacing: 0.02em;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.white};
+`
