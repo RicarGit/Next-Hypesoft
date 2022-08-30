18b17d1d5a768e2ac369c05e3fdcdfeb6ce75017
Ricardo Gava <ricardoagava@gmail.com>
2022-08-30 11:07:35 -0300

    feat(title): added a title to home page's head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={defautTheme}>
       <GlobalStyle />
       <Head>
+        <title>
+          Home - Hypesoft
+        </title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
