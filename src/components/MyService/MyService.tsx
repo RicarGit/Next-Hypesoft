0c72c6fe0e3113759f019f400e3fe976888c85d5
Ricardo Gava <ricardoagava@gmail.com>
2022-10-07 04:13:00 -0300

    style: removed some white spaces from MyService component

--- src/components/MyService/MyService.tsx
+++ src/components/MyService/MyService.tsx
@@ -18,21 +18,18 @@ export const MyService = () => {
             info2='Design'
             alt='layers icon'
           />
-
           <ServiceCard
             icon={Assets.PinkBoxIcon}
             info1='Product'
             info2='Design'
             alt='pink box icon' className='active'
           />
-
           <ServiceCard
             icon={Assets.ColumnsIcon}
             info1='Branding'
             info2='Design'
             alt='columns icon'
           />
-
           <ServiceCard
             icon={Assets.CodeIcon}
             info1='Front End'
